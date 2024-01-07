cdc9e762fc3037fa8bb7df5993ea97f1b23fdf9a
Sean Li <hi@sean.li>
2024-01-07 15:27:41 -0800

    Add ABI generation for deployment

--- tasks/deploy.ts
+++ tasks/deploy.ts
@@ -1,6 +1,39 @@
 import { task } from "hardhat/config";
 import { formatEther, parseEther } from "viem";
 
+const fs = require("fs");
+const path = require("path");
+
+const getContractABI = () => {
+    try {
+        const dir = path.resolve(
+            __dirname,
+            "../artifacts/contracts/RedPacket.sol/RedPacket.json"
+        )
+        const file = fs.readFileSync(dir, "utf8")
+        const json = JSON.parse(file)
+        const abi = json.abi
+        console.log("abi", abi)
+
+        return abi
+    } catch (e) {
+        console.log(`e`, e)
+    }
+}
+
+function saveContractABIFile() {
+    const contractsDir = path.join(__dirname, "..", "dapp", "src", "contracts");
+
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
+
+    fs.writeFileSync(
+        path.join(contractsDir, "RedPacket.json"),
+        JSON.stringify(getContractABI(), null, 2)
+    );
+}
+
 // npx hardhat deploy --count 3 --balance 10 --network localhost
 // npx hardhat deploy --count 3 --balance 0.01 --network sepolia
 task("deploy", "Deploy a RedPacket contract with custom parameters")
@@ -22,6 +55,8 @@ task("deploy", "Deploy a RedPacket contract with custom parameters")
     const packet = await hre.viem.deployContract("RedPacket", [count], {
         value: balance,
     });
+
+    saveContractABIFile();
 
     console.log(
         `RedPacket with ${formatEther(balance)} ETH and ${count} users deployed to ${packet.address}`
