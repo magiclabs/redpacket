6767b7a5d4f9edab13da6bf649c54a6a6220ddb5
Sean Li <hi@sean.li>
2024-01-07 15:09:26 -0800

    Add dapp file with AA

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,14 +4,14 @@ import "@nomicfoundation/hardhat-toolbox-viem";
 require('@openzeppelin/hardhat-upgrades');
 require("./tasks/deploy");
 
-const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     }
   }
