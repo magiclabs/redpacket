b92d4f569d0cc921e4acbaa456fae8582bef5c19
Sean Li <hi@sean.li>
2024-01-09 08:58:05 -0800

    Move links out of card

--- client/src/components/Packet.tsx
+++ client/src/components/Packet.tsx
@@ -48,94 +48,96 @@ export default function Packet({ contractAddress }: PacketProps) {
     }, [logout]);
     
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
-            <figure><img src="/red-packet.png" alt="Red Packet" /></figure>
-                <div className="card-body">
-                    {loading ? (
-                        <div>
-                            <div className="flex flex-col gap-3 w-full">
-                                <div className="skeleton h-3 w-full"></div>
-                                <div className="skeleton h-3 w-full"></div>
-                                <div className="skeleton h-3 w-full"></div>
-                                <div className="skeleton h-12 w-full"></div>
-                            </div>
-                        </div>
-                    ) : (
-                        <div>
-                            {claimedAmount ? (
-                                <div>
-                                    <h1 className="card-title">You've claimed {formatEtherDisplay(claimedAmount)} ETH.</h1>
-                                    {expired ? (
-                                        <div className="text-md mt-6">
-                                            💸 All red packets have been claimed.
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            <progress className="progress progress-primary w-full progress-lg h-3 mt-6" value={`${currentBalance as bigint}`} max={`${totalBalance as bigint}`}></progress>
-                                            <div className="text-xs">{`${formatEtherDisplay(currentBalance as bigint)} / ${formatEtherDisplay(totalBalance as bigint)} ETH left to claim by ${userDisplay(remainingClaimCount as bigint)}.`}</div>
-                                        </div>
-                                    )}
+        <div>
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <figure><img src="/red-packet.png" alt="Red Packet" /></figure>
+                    <div className="card-body">
+                        {loading ? (
+                            <div>
+                                <div className="flex flex-col gap-3 w-full">
+                                    <div className="skeleton h-3 w-full"></div>
+                                    <div className="skeleton h-3 w-full"></div>
+                                    <div className="skeleton h-3 w-full"></div>
+                                    <div className="skeleton h-12 w-full"></div>
                                 </div>
-                            ) : (
-                                <div>
-                                    {expired ? (
-                                        <div>
-                                            <h1 className="card-title">You're too late!</h1>
-                                            <div className="text-md mt-3">
+                            </div>
+                        ) : (
+                            <div>
+                                {claimedAmount ? (
+                                    <div>
+                                        <h1 className="card-title">You've claimed {formatEtherDisplay(claimedAmount)} ETH.</h1>
+                                        {expired ? (
+                                            <div className="text-md mt-6">
                                                 💸 All red packets have been claimed.
                                             </div>
-                                        </div>
-                                    ) : (
-                                        <div>
-                                            <h1 className="card-title">🧧 You received a red packet!</h1>
-                                            <progress className="progress progress-primary w-full progress-lg h-3 mt-6" value={`${currentBalance as bigint}`} max={`${totalBalance as bigint}`}></progress>
-                                            <div className="text-xs">{`${formatEtherDisplay(currentBalance as bigint)} / ${formatEtherDisplay(totalBalance as bigint)} ETH left to claim by ${userDisplay(remainingClaimCount as bigint)}.`}</div>
-                                            <div className="card-actions justify-end mt-6">
-                                                {isLoggedIn ? (
-                                                    <div className="w-full">
-                                                        {isClaiming ? (
-                                                            <button disabled={true} className="btn btn-primary btn-block btn-lg">
-                                                                Claiming
-                                                                <span className="loading loading-infinity loading-lg"></span>
-                                                            </button>
-                                                        ) : (
-                                                            <button onClick={handleClaim} className="btn btn-primary btn-block btn-lg">Claim</button>
-                                                        )}
-                                                    </div>
-                                                ) : (
-                                                    <div className="join">
-                                                        <input onChange={onEmailChange} className="input input-bordered join-item w-full" placeholder="Your Email"/>
-                                                        <button onClick={handleLogin} className="btn btn-primary join-item">Login to Claim</button>
-                                                    </div>
-                                                )}
+                                        ) : (
+                                            <div>
+                                                <progress className="progress progress-primary w-full progress-lg h-3 mt-6" value={`${currentBalance as bigint}`} max={`${totalBalance as bigint}`}></progress>
+                                                <div className="text-xs">{`${formatEtherDisplay(currentBalance as bigint)} / ${formatEtherDisplay(totalBalance as bigint)} ETH left to claim by ${userDisplay(remainingClaimCount as bigint)}.`}</div>
                                             </div>
-                                        </div>
-                                    )}
-                                </div>
-                            )}
-                            <div className="text-center mt-3">
-                                <a href={`https://basescan.org/address/${contractAddress}`} target="_blank" className="btn btn-link text-base-300 btn-xs">
-                                    View Contract
-                                </a>
-                                {claimTxnHash && (
-                                    <a href={`https://basescan.org/tx/${claimTxnHash}`} target="_blank" className="btn btn-link text-base-300 btn-xs">
-                                        View Transaction
-                                    </a>
-                                )}
-                                {isLoggedIn && (
+                                        )}
+                                    </div>
+                                ) : (
                                     <div>
-                                        <a href={`https://basescan.org/address/${scaAddress}`} target="_blank" className="btn btn-link text-base-300 btn-xs">
-                                            View Wallet
-                                        </a>
-                                        <a onClick={handleLogout} target="_blank" className="btn btn-link text-base-300 btn-xs">
-                                            Logout: {username}
-                                        </a>
+                                        {expired ? (
+                                            <div>
+                                                <h1 className="card-title">You're too late!</h1>
+                                                <div className="text-md mt-3">
+                                                    💸 All red packets have been claimed.
+                                                </div>
+                                            </div>
+                                        ) : (
+                                            <div>
+                                                <h1 className="card-title">🧧 You received a red packet!</h1>
+                                                <progress className="progress progress-primary w-full progress-lg h-3 mt-6" value={`${currentBalance as bigint}`} max={`${totalBalance as bigint}`}></progress>
+                                                <div className="text-xs">{`${formatEtherDisplay(currentBalance as bigint)} / ${formatEtherDisplay(totalBalance as bigint)} ETH left to claim by ${userDisplay(remainingClaimCount as bigint)}.`}</div>
+                                                <div className="card-actions justify-end mt-6">
+                                                    {isLoggedIn ? (
+                                                        <div className="w-full">
+                                                            {isClaiming ? (
+                                                                <button disabled={true} className="btn btn-primary btn-block btn-lg">
+                                                                    Claiming
+                                                                    <span className="loading loading-infinity loading-lg"></span>
+                                                                </button>
+                                                            ) : (
+                                                                <button onClick={handleClaim} className="btn btn-primary btn-block btn-lg">Claim</button>
+                                                            )}
+                                                        </div>
+                                                    ) : (
+                                                        <div className="join">
+                                                            <input onChange={onEmailChange} className="input input-bordered join-item w-full" placeholder="Your Email"/>
+                                                            <button onClick={handleLogin} className="btn btn-primary join-item">Login to Claim</button>
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            </div>
+                                        )}
                                     </div>
                                 )}
                             </div>
-                        </div>
-                    )}
-                </div>
+                        )}
+                    </div>
+            </div>
+            <div className="text-center mt-6">
+                <a href={`https://basescan.org/address/${contractAddress}`} target="_blank" className="btn btn-link btn-block text-base-300 btn-xs">
+                    View Contract
+                </a>
+                {claimTxnHash && (
+                    <a href={`https://basescan.org/tx/${claimTxnHash}`} target="_blank" className="btn btn-link btn-block text-base-300 btn-xs">
+                        View Transaction
+                    </a>
+                )}
+                {isLoggedIn && (
+                    <div>
+                        <a href={`https://basescan.org/address/${scaAddress}`} target="_blank" className="btn btn-link btn-block text-base-300 btn-xs">
+                            View Wallet
+                        </a>
+                        <a onClick={handleLogout} target="_blank" className="btn btn-link text-base-300 btn-xs">
+                            Logout: {username}
+                        </a>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
