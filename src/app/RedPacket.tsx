514f59456528ef6c2b1b3f122381afc34a150620
Jay Hwang <octave08b@gmail.com>
2024-02-03 23:33:16 +0900

    Update animation

--- src/app/RedPacket.tsx
+++ src/app/RedPacket.tsx
@@ -1,17 +1,21 @@
 import { MotionImage, type MotionImageProps } from 'components/MotionImage'
+import { cn } from 'lib/utils'
 
-export function RedPacket(props: MotionImageProps) {
+export function RedPacket({ className, ...rest }: MotionImageProps) {
   return (
     <MotionImage
       priority
       key="red-packet"
-      className="z-20 mt-4 aspect-square h-56 w-56 rotate-[30deg] select-none sm:mt-12 md:h-64 md:w-64"
+      className={cn(
+        'z-20 mt-4 aspect-square h-56 w-56 rotate-[30deg] select-none sm:mt-12 md:h-64 md:w-64',
+        className,
+      )}
       src="/Red_Envelope_1.png"
       width="1000"
       height="1000"
       quality={80}
       alt="Red Packet"
-      {...props}
+      {...rest}
     />
   )
 }
---
+++ src/app/claim/[key]/layout.tsx
@@ -0,0 +1,14 @@
+import { BackLanturns } from 'app/claim/BackLanterns'
+import { RedFocus } from 'app/claim/RedFocus'
+import { type PropsWithChildren } from 'react'
+
+export default function ClaimLayout({ children }: PropsWithChildren) {
+  return (
+    <>
+      <BackLanturns />
+      <RedFocus />
+
+      {children}
+    </>
+  )
+}
