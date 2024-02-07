6f5e7ceb036944cf1249398a0a21c434fedea86e
ysm-dev <i@ysm.dev>
2024-02-07 12:49:51 +0900

    Hide walletdropdown in home

--- src/app/(create)/layout.tsx
+++ src/app/(create)/layout.tsx
@@ -4,13 +4,16 @@ import { WalletDropdown } from 'app/(create)/create/WalletDropdown'
 import { DemoByMagic } from 'app/DemoByMagic'
 import { BackLanturns } from 'app/claim/BackLanterns'
 import { RedFocus } from 'app/claim/RedFocus'
-import { useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation'
 import { useEffect, type PropsWithChildren } from 'react'
 import { useAccount } from 'wagmi'
 
 export default function CreateLayout({ children }: PropsWithChildren) {
   const { push } = useRouter()
   const { isConnecting, isConnected, isReconnecting } = useAccount()
+  const pathname = usePathname()
+
+  const isHome = pathname === '/'
 
   useEffect(() => {
     if (isConnecting || isReconnecting) {
@@ -30,7 +33,7 @@ export default function CreateLayout({ children }: PropsWithChildren) {
     <main className="relative flex h-dvh w-full max-w-full flex-col items-center overflow-x-hidden">
       <BackLanturns />
       <RedFocus />
-      <WalletDropdown />
+      {!isHome && <WalletDropdown />}
 
       {children}
 
