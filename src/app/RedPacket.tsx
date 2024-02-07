4841e6bada9e76e6aef5dfa88fdf44a9c1d65eae
Jay Hwang <octave08b@gmail.com>
2024-02-07 11:07:59 +0900

    Update envelope image

--- src/app/RedPacket.tsx
+++ src/app/RedPacket.tsx
@@ -10,7 +10,7 @@ export function RedPacket({ className, ...rest }: MotionImageProps) {
         'z-20 mt-4 aspect-square h-[224px] w-[224px] rotate-[30deg] select-none sm:mt-12 sm:h-[260px] sm:w-[260px]',
         className,
       )}
-      src="/Red_Envelope_1.png"
+      src="/red-envelope.png"
       width="1000"
       height="1000"
       quality={80}
