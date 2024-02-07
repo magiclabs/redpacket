87be853cfd1b74d5f62fb7b1c9cda2b5cfe2fe32
Jay Hwang <octave08b@gmail.com>
2024-02-07 22:46:46 +0900

    Fix hydration issue

--- src/app/WagmiAuth.tsx
+++ src/app/WagmiAuth.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { InfiniteLoadingSpinner } from 'components/icons/InfiniteLoadingSpinner'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useEffect, useState, type PropsWithChildren } from 'react'
 import { isServer } from 'utils/isServer'
 import { useAccount } from 'wagmi'
@@ -14,30 +14,44 @@ export const WagmiAuth = ({ children }: PropsWithChildren) => {
     setIsMounted(true)
   }, [])
 
-  if (isServer() || isReconnecting || !isMounted) {
-    return (
-      <main className="flex h-dvh w-full flex-col items-center justify-center overflow-x-hidden">
-        <div className="flex flex-col items-center">
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 1, duration: 1 }}
-          >
+  return (
+    <AnimatePresence mode="wait" initial={false}>
+      {isServer() || isReconnecting || !isMounted ? (
+        <motion.main
+          key="loading"
+          className="flex h-dvh w-full flex-col items-center justify-center overflow-x-hidden"
+          initial={{ opacity: 1 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          <div className="flex flex-col items-center">
             <InfiniteLoadingSpinner className="aspect-square h-16 w-16" />
-          </motion.div>
-          <span className="text-sm font-semibold">Loading Red Packets...</span>
-          <motion.span
-            className="mt-1 text-center text-sm text-[#ffffffcc]"
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 2, duration: 0.5 }}
-          >
-            This takes a few seconds
-          </motion.span>
-        </div>
-      </main>
-    )
-  }
-
-  return <>{children}</>
+            <span className="text-sm font-semibold">
+              Loading Red Packets...
+            </span>
+            {isMounted && (
+              <motion.span
+                className="mt-1 text-center text-sm text-[#ffffffcc]"
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ delay: 2, duration: 0.5 }}
+              >
+                This takes a few seconds
+              </motion.span>
+            )}
+          </div>
+        </motion.main>
+      ) : (
+        <motion.div
+          key="content"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          {children}
+        </motion.div>
+      )}
+    </AnimatePresence>
+  )
 }
