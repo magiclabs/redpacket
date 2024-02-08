2919193e628bd199d57222db3fa9fb1f508a6ae4
Jay Hwang <octave08b@gmail.com>
2024-02-09 06:29:43 +0900

    Update wording

--- src/app/claim/[key]/over/page.tsx
+++ src/app/claim/[key]/over/page.tsx
@@ -1,22 +1,17 @@
 'use client'
 
 import { track } from '@vercel/analytics/react'
-import { useRedPacket } from 'app/(create)/share/[key]/useRedPacket'
 import { RedLantern } from 'app/claim/[key]/over/RedLantern'
 import { Button } from 'components/ui/button'
 import { Container } from 'components/ui/container'
 import { MotionHeadline } from 'components/ui/typography'
 import { motion } from 'framer-motion'
 import { ANIMATION_INTERVAL } from 'lib/constants'
-import { useParams, useRouter } from 'next/navigation'
-import { type Address } from 'viem'
+import { useRouter } from 'next/navigation'
 import { useAccount, useDisconnect } from 'wagmi'
 
 export default function Over() {
   const { push } = useRouter()
-  const { key } = useParams<{ key: string }>()
-
-  const contractAddress: Address = `0x${key}`
 
   const { isConnected } = useAccount()
   const { disconnect } = useDisconnect({
@@ -28,8 +23,6 @@ export default function Over() {
     },
   })
 
-  const { totalClaimCount } = useRedPacket({ contractAddress })
-
   return (
     <Container>
       <RedLantern />
@@ -40,33 +33,24 @@ export default function Over() {
         animate={{ opacity: 1 }}
         transition={{ delay: ANIMATION_INTERVAL * 1 }}
       >
-        The festivities <span className="inline-block">are over</span>
+        All packets claimed
       </MotionHeadline>
 
-      <motion.p
-        className="mt-5 text-center text-lg font-normal leading-normal tracking-[-0.408px] text-[#ffffffcc] opacity-80 sm:text-lg"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.8 }}
-        transition={{ delay: ANIMATION_INTERVAL * 2 }}
-      >
-        All {totalClaimCount.toLocaleString()} red packets have been claimed.
-      </motion.p>
-
       <motion.div
-        className="mt-5 text-balance text-center text-base font-normal leading-normal tracking-[-0.408px] text-[#ffffffcc]"
+        className="mt-8 text-balance text-center text-lg font-normal leading-normal tracking-[-0.408px] text-[#ffffffcc]"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: ANIMATION_INTERVAL * 3 }}
+        transition={{ delay: ANIMATION_INTERVAL * 2 }}
       >
-        Use the link below to watch a video demo of the experience, or{' '}
-        <span className="font-semibold text-white">build your own.</span>
+        Use the link below to gift red packets to your friends and family this
+        Lunar New Year!
       </motion.div>
 
       <motion.div
-        className="mt-8 flex w-full flex-col gap-3 sm:mt-10 sm:flex-row"
+        className="mt-10 flex w-full flex-col gap-3 sm:mt-10 sm:flex-row"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ delay: ANIMATION_INTERVAL * 4 }}
+        transition={{ delay: ANIMATION_INTERVAL * 3 }}
       >
         {isConnected && (
           <Button
