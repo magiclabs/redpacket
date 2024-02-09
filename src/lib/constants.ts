e2e95e753b30e49e1434ad12c85fc9864374a9b7
Karl Jin <7002536+karljin@users.noreply.github.com>
2024-02-09 12:53:52 -0600

    [FIX] Changing constants (#26)
    Need to follow up with actually getting the values from the .env

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -9,12 +9,12 @@ export const REDPACKET_ABI = RedPacket.abi
 export const ANIMATION_INTERVAL = 0.5
 
 export const ALCHEMY_GASMANAGER_POLICY_ID = {
-  base: `345b7869-b269-4000-af0f-1a6c98f8a0a4`,
+  base: `ab7e61d7-e32d-4678-bf92-0cf6b2e1b5aa`,
   mumbai: `652ddbb1-2159-4926-8ea4-1effaa2b7658`,
 } as const
 
 export const ALCHEMY_RPC_URL = {
-  base: `https://base-mainnet.g.alchemy.com/v2/EzC7OYuylt0HecPRiyZX-HMNaYJiHy9j`,
+  base: `https://base-mainnet.g.alchemy.com/v2/mgAiAkPBUrNakqSTikVs4IYvve4AVh07`,
   baseSepolia: `https://base-sepolia.g.alchemy.com/v2/8i288JRIGXoG0UUJWfFwrucu0BRHbb1-`,
   mumbai: `https://polygon-mumbai.g.alchemy.com/v2/Sj_KlZV6R-XvaKewU9KTuSOvZXVNSL4j`,
 } as const
