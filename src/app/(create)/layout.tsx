3eca5604c3369066a292f22c2bdda667f521dba5
Jay Hwang <octave08b@gmail.com>
2024-02-15 19:57:06 +0900

    Add dead-end page

--- src/app/(create)/layout.tsx
+++ src/app/(create)/layout.tsx
@@ -4,8 +4,10 @@ import { last, pipe, split } from '@fxts/core'
 import { BackLanturns } from 'components/BackLanterns'
 import { RedFocus } from 'components/RedFocus'
 import { WalletDropdown } from 'components/WalletDropdown'
-import { usePathname } from 'next/navigation'
+import { redirect, usePathname } from 'next/navigation'
 import { useMemo, type PropsWithChildren } from 'react'
+
+const DEAD_LINE = process.env.NEXT_PUBLIC_DEAD_LINE
 
 export default function CreateLayout({ children }: PropsWithChildren) {
   const pathname = usePathname()
@@ -14,6 +16,10 @@ export default function CreateLayout({ children }: PropsWithChildren) {
     const subpath = pipe(pathname, split('/'), last)
     return subpath !== 'login'
   }, [pathname])
+
+  if (pathname !== '/dead' && DEAD_LINE && new Date() > new Date(DEAD_LINE)) {
+    redirect('/dead')
+  }
 
   return (
     <>
