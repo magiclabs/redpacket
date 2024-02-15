171eb49f9720c73be8c76b3c4b3b6dd9fef85184
ysm-dev <i@ysm.dev>
2024-02-15 23:58:20 +0900

    Fix deploy script baseURL

--- src/utils/isLocal.tsx
+++ src/utils/isLocal.tsx
@@ -2,7 +2,10 @@ import { isServer } from 'utils/isServer'
 
 export const isLocal = () => {
   if (isServer()) {
-    return process.env.NODE_ENV === 'development'
+    return (
+      process.env.NODE_ENV === 'development' ||
+      process.env.VERCEL_ENV === 'development'
+    )
   } else {
     return window.location.hostname === 'localhost'
   }
